b4269b721d785db255446cd5e601317d63ddd39c
MINH <minhdoan983@gmail.com>
2024-07-26 23:03:27 +0700

    them file a

--- src/auth/RequireAuth.js
+++ src/auth/RequireAuth.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import  { useContext } from "react";
+import { useLocation } from "react-router-dom";
 import AuthContext from "./AuthContext";
 
 function RequireAuth({ children }) {
